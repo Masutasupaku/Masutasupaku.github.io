7b1f681596c3d665e29f503e40770ddbb62835ab
wmichaelv <wmichaelv@hotmail.com>
2014-10-25 00:09:33 -0700

    update spell card

--- db/migrate/20141016193620_create_spell_cards.rb
+++ db/migrate/20141016193620_create_spell_cards.rb
@@ -6,6 +6,36 @@ class CreateSpellCards < ActiveRecord::Migration
       t.integer :iv
       t.integer :hv
       t.integer :sp
+      t.boolean :has_basic_ability
+      t.integer :focus_movement
+      t.integer :unfocus_movement
+      t.integer :faith
+      t.integer :protection
+      t.boolean :guided_bullet
+      t.boolean :penetration
+      t.boolean :can_activate
+      t.boolean :can_battle
+      t.boolean :can_reserve
+      t.boolean :has_extra_cost_activate
+      t.integer :extra_cost_activate_hp
+      t.integer :extra_cost_activate_sp
+      t.integer :extra_cost_activate_deck_card
+      t.integer :extra_cost_activate_hand_card
+      t.boolean :has_extra_cost_battle
+      t.integer :extra_cost_battle_hp
+      t.integer :extra_cost_battle_sp
+      t.integer :extra_cost_battle_deck_card
+      t.integer :extra_cost_battle_hand_card
+      t.boolean :has_special
+      t.boolean :can_use_active
+      t.boolean :can_use_passive
+      t.boolean :targetable_by_own_special
+      t.boolean :targetable_by_own_support
+      t.boolean :targetable_by_own_event
+      t.boolean :targetable_by_opp_special
+      t.boolean :targetable_by_opp_support
+      t.boolean :targetable_by_opp_event
+
       t.string :spell_type
       t.references :card
 
