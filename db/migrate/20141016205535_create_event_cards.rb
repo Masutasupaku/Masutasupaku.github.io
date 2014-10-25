50431e2598e09081b3a8250b9027afd6046abefe
wmichaelv <wmichaelv@hotmail.com>
2014-10-25 01:08:53 -0700

    update event card

--- db/migrate/20141016205535_create_event_cards.rb
+++ db/migrate/20141016205535_create_event_cards.rb
@@ -3,7 +3,21 @@ class CreateEventCards < ActiveRecord::Migration
 	create_table :event_cards do |t|
 		t.string :cid
 		t.integer :sp
-		t.string :phase
+		t.boolean :has_restriction
+		t.string  :require_character
+		t.string  :require_designation
+		t.integer :require_level
+		t.string  :require_phase
+		t.string  :require_side
+		t.boolean :once_per_turn
+		t.boolean :has_extra_cost_use
+		t.boolean :extra_cost_use_hp
+		t.boolean :extra_cost_use_sp
+		t.boolean :extra_cost_use_deck_card
+		t.boolean :extra_cost_use_field_card
+		t.boolean :extra_cost_use_hand_card
+		t.boolean :extra_cost_use_reserve_active
+		t.string  :phase
 		t.references :card
 		
 		t.timestamps
